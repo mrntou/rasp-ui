f771d6f3ba23efe378354585fca0219a1693e63c
mrntou <mr.ntou10@gmail.com>
2022-06-27 00:53:35 +0300

    00:59 INBC

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import QTimer, QTime, Qt
 import sys
+from datetime import datetime
 
 
 class Ui(QtWidgets.QMainWindow):
@@ -10,16 +11,33 @@ class Ui(QtWidgets.QMainWindow):
         self.show() # Show the GUI
 
         timer = QTimer(self)
-        timer.timeout.connect(self.displayTime)
+        timer.timeout.connect(self.displayHour)
+        timer.timeout.connect(self.displaySec)
+        timer.timeout.connect(self.displayDate)
+
+
         timer.start(1000)
 
+        self.closeBtn.clicked.connect(sys.exit)
 
-    def displayTime(self):
+
+    def displayHour(self):
         currentTime = QTime.currentTime()
-        displayText = currentTime.toString("hh:mm:ss")
-        print(currentTime)
+        displayText = currentTime.toString("hh:mm")
 
-        self.label.setText(displayText)
+        self.hour.setText(displayText)
+
+    def displaySec(self):
+        currentTime = QTime.currentTime()
+        displayText = currentTime.toString("ss")
+
+        self.sec.setText(displayText)
+
+    def displayDate(self):
+        today = datetime.today().strftime("%A")
+        self.day.setText(today)
+
+
 
 
 app = QtWidgets.QApplication(sys.argv)
